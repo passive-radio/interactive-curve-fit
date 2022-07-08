9658442050100a2f48adff5be5426de6cb4c8a08
passive-radio <srccreator@gmail.com>
2022-07-08 11:50:36 +0900

    updated fittng.py, guessing.py, fitting_functions.py

--- how_to_use.py
+++ how_to_use.py
@@ -1,7 +1,7 @@
 from core.preprocessing import read_data, reset_range
-from utils.bmptocsv import bmp_to_csv
-from core.interactive import Guessor
-from core.detection import Fitter
+from utils.imgToCSV import bmp_to_csv
+from core.guessing import Guessor
+from core.fitting import Fitter
 
 
 def main():
@@ -10,12 +10,13 @@ def main():
     data = read_data('sample_data/sample.csv', 0, ',')
     
     guessor = Guessor(data, background=0, method="drag")
-    guess = guessor.guess()
+    init_guess = guessor.guess()
 
-    optimizer = Fitter(data, guess)
-    optimizer.run()
-    optimizer.save_data(save_path='result/spectrum0.csv')
+    optimizer = Fitter(data, init_guess)
+    optimizer.run(method= 'gaussian')
+    optimizer.save_data(save_path='out/spectrum0.csv')
     optimizer.display_results_terminal(ci=2)
+    optimizer.plot_fit()
 
 if __name__ == "__main__":
     
