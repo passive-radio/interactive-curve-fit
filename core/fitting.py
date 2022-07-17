e5e9493354e28a19bc4a2d54129ac5be74e03ad9
passive-radio <srccreator@gmail.com>
2022-07-17 18:46:38 +0900

    updated fitting, fitting_functions

--- core/fitting.py
+++ core/fitting.py
@@ -54,82 +54,30 @@ class Fitter(object):
     """
     def fit_by_gaussian_func(self):
         
-        def exp_func(x=self.data.x, *params_guess):
-            params = params_guess
-            #paramsの長さでフィッティングする関数の数を判別。
-            num_func = int(len(params)/3)
-            self.num_peaks = num_func
-            
-            #ガウス関数にそれぞれのパラメータを挿入してy_listに追加。
-            y_list = []
-            for i in range(num_func):
-                y = np.zeros_like(x)
-                param_range = list(range(3*i,3*(i+1),1))
-                ctr = params[int(param_range[0])]
-                amp = params[int(param_range[1])]
-                wid = params[int(param_range[2])]
-                y = y + amp * np.exp( -((x - ctr)/wid)**2)
-                y_list.append(y)
-
-            #y_listに入っているすべてのガウス関数を重ね合わせる。
-            y_sum = np.zeros_like(x)
-            for i in y_list:
-                y_sum = y_sum + i
-
-            #最後にバックグラウンドを追加。
-            y_sum = y_sum + params[-1]
-
-            return y_sum
+        from .fitting_functions import gaussian_func
         
         guess_list = []
-        for content in self.guess[:-1]:
-            guess_list.extend(content)
+        for val in self.guess[:-1]:
+            guess_list.extend(val)
         guess_list.append(self.guess[-1])
+        self.num_peaks = int((len(guess_list)-1)/3)
         
-        popt, pcov = curve_fit(exp_func, self.data.x, self.data.y, p0=guess_list)
-        self.popt = popt
-        self.pcov = pcov
-        return popt, pcov
+        self.popt, self.pcov = curve_fit(gaussian_func, self.data.x, self.data.y, p0=guess_list)
+        return self.popt, self.pcov
     
     def fit_by_polynomial_func(self):
         
+        from .fitting_functions import Polynomial
+        polynomial = Polynomial(deg=self.deg)
+        
         guess_list = []
         for content in self.guess[:-1]:
             guess_list.extend(content)
         guess_list.append(self.guess[-1])
-
-        def plynomi_func(x=self.data.x, *params_guess):
-            # params_guess = [[pos, amp1, pos, amp2],[pos, amp1, pos, amp2],...]
-            if type(params_guess)==list:
-                params = params_guess
-            elif type(params_guess)==tuple:
-                params = list(params_guess)
-            
-            num_func = int((len(params_guess)-1)/(self.deg*2))
-            self.num_peaks = num_func
-            y_list = []
-            for i in range(num_func):
-                y = np.zeros_like(x)
-                for j in range(i, self.deg):
-                    # params[2+deg*i + 2*j+1]       amp
-                    # params[2+deg*i + 2*j]         pos
-                    # j+1                           deg
-                    y = y + np.array(params[2*self.deg*i + 2*j+1] * (x-params[2*self.deg*i + 2*j]) ** (j+1), dtype="float64")
-                y_list.append(y)
-
-            y_sum = np.zeros_like(x)
-            for i in y_list:
-                y_sum = y_sum + i
-                
-            # add background
-            y_sum = y_sum + params[-1]
-            return y_sum
+        self.num_peaks = int((len(guess_list)-1)/(self.deg*2))
         
-        # return plynomi_func(x, params_new)
-        popt, pcov = curve_fit(plynomi_func, self.data.x, self.data.y, p0=guess_list)
-        self.popt = popt
-        self.pcov = pcov
-        return popt, pcov
+        self.popt, self.popv = curve_fit(polynomial.polynomial_func, self.data.x, self.data.y, p0=guess_list, )
+        return self.popt, self.popv
     
     def plot_fit(self):
         x = self.data.x
--- how_to_use.py
+++ how_to_use.py
@@ -3,18 +3,22 @@ from utils.imgToCSV import bmp_to_csv
 from core.guessing import Guessor
 from core.fitting import Fitter
 
+from utils.draw_test_data import draw_test
 
 def main():
     
-    bmp_to_csv('sample_data/sample.bmp')
-    data = read_data('sample_data/sample.csv', 0, ',')
+    # bmp_to_csv('sample_data/sample.bmp')
+    draw_test('sample_data/drawed_function.csv')
+    data = read_data('sample_data/drawed_function.csv', 0, ',')
     
     guessor = Guessor(data, background=0, method="drag")
     init_guess = guessor.guess()
+    
+    print(type(init_guess))
 
     optimizer = Fitter(data, init_guess)
-    optimizer.run(method= 'gaussian')
-    optimizer.save_data(save_path='out/spectrum0.csv')
+    optimizer.run(method= 'polynomial', deg=2)
+    optimizer.save_data(save_path='out/spectrum1.csv')
     optimizer.display_results_terminal(ci=2)
     optimizer.plot_fit()
 
